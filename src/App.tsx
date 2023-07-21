cc7d4ae33ee00a652e4ad45e6382ce2b2caf1c66
MRAlirad <m.r.alirad75@gmail.com>
2023-07-21 21:20:33 +0330

    index

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Button, ButtonGroup } from '@chakra-ui/react';
 
 function App()
 {
-  return (
-    <Button colorScheme='blue'>Button</Button>
-  )
+	return (
+		<Button colorScheme='blue'>Button</Button>
+	)
 }
 
 export default App
