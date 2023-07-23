9a352d43425ede68b2c6c92f333ae519e2078c3f
MRAlirad <m.r.alirad75@gmail.com>
2023-07-24 00:17:15 +0330

    displaying platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ const GameGrid = ()=> {
         <div>
             {error && <p>{error}</p> }
             <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} spacing={10} padding="10px">
-                {games.map(game => <GameCard game={game}/>)}
+                {games.map(game => <GameCard game={game} key={game.id}/>)}
             </SimpleGrid>
         </div>
     );
