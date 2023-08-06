3c25fdec1778354f54e5431fc44044d7ea094990
MRAlirad <m.r.alirad75@gmail.com>
2023-08-06 21:24:34 +0330

    refactoring a reuseable API

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { FetchResponse } from "../services/api-client";
-import apiClient from "../services/api-client";
 import { Platform } from "./usePlatforms";
+import APIClient from "../services/api-client";
+
+const apiClient = new APIClient<Game>('/games');
 
 export interface Game {
     id : number;
@@ -17,16 +19,14 @@ const useGames = (gameQuery : GameQuery)=>
     useQuery<FetchResponse<Game>, Error>({
         queryKey : ['games', gameQuery],
         queryFn  : ()=>
-            apiClient
-                .get<FetchResponse<Game>>('/games', {
-                    params : {
-                        genre: gameQuery.genre?.id,
-                        platform : gameQuery.platform?.id,
-                        ordering : gameQuery.sortOrder,
-                        search: gameQuery.searchText
-                    }
-                })
-                .then(res => res.data)
+            apiClient.getAll({
+                params : {
+                    genre: gameQuery.genre?.id,
+                    platform : gameQuery.platform?.id,
+                    ordering : gameQuery.sortOrder,
+                    search: gameQuery.searchText
+                },
+            }),
     })
 ;
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,31 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
     count : number;
     results : T[]
 }
 
-export default axios.create({
+const axiosInstance =  axios.create({
     baseURL: 'https://api.rawg.io/api',
     params : {
         key : '0c92309b3b4347f7b054cd14440fd37d',
     }
 });
+
+class APIClient<T> {
+    endpoint : string;
+
+    constructor(endpoint: string)
+    {
+        this.endpoint = endpoint;
+    }
+
+    getAll = async (config : AxiosRequestConfig)=>
+    {
+        return axiosInstance
+            .get<FetchResponse<T>>(this.endpoint, config)
+            .then(res => res.data)
+    }
+}
+
+export default APIClient;
