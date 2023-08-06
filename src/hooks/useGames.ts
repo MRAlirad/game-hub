6abb2c8af5d9b532689ae3de8c574714c6f89b5e
MRAlirad <m.r.alirad75@gmail.com>
2023-08-07 00:19:10 +0330

    simplify time calculation

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
-import { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
-import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -29,7 +29,7 @@ const useGames = (gameQuery : GameQuery)=>
                 },
             }),
         getNextPageParam: (lastPage, allPages)=> lastPage.next ? allPages.length + 1 : undefined,
-        staleTime : 24 * 60 * 60 * 1000 //24h
+        staleTime : ms('24h'),
     })
 ;
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
@@ -12,7 +13,7 @@ export interface Platform {
 const usePlatforms = ()=> useQuery({
     queryKey : ['platforms'],
     queryFn  : apiClient.getAll,
-    staleTime : 24 * 60 * 60 * 1000,
+    staleTime : ms('24h'),
     // initialData : {count : platforms.length, results: platforms, next: null}
 })
 
