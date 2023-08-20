a4f86648a86bdff440f809b3552edbff969b8854
MRAlirad <m.r.alirad75@gmail.com>
2023-08-20 19:51:08 +0330

    error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,25 @@
+import { Box, Heading, Text } from "@chakra-ui/react"
+import { isRouteErrorResponse, useRouteError } from "react-router-dom"
+import NavBar from "../components/NavBar"
+
+const ErrorPage = () => {
+    const error = useRouteError()
+    return (
+        <>
+            <NavBar/>
+            <Box margin={5}>
+                <Heading>Oops</Heading>
+                <Text>
+                    {
+                        isRouteErrorResponse(error) ?
+                            'This page does not exist'
+                        :
+                            'An unexpected error happened'
+                    }
+                </Text>
+            </Box>
+        </>
+    )
+}
+
+export default ErrorPage
